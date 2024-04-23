9d3a86d5c9a2e8bb0f024d5897389db718b4f674
Barbara Mani <barbaramani@outlook.com>
2024-04-23 19:03:09 -0300

    primeiras aulas sql

---
+++ ep01/hello.sql
@@ -0,0 +1,16 @@
+ SELECT product_id,
+       product_category_name,
+       product_photos_qty
+
+
+
+ from tb_products
+
+ WHERE ( product_category_name = 'bebes'
+ or product_category_name = 'perfumaria')
+
+and product_photos_qty > 1
+
+
+
+
